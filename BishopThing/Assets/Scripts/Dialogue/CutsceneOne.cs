c02566a41ace97803cfbdf04a253292cf629fdd2
inkpin <148596974+inkpin@users.noreply.github.com>
2026-02-27 23:50:02 +0000

    cutscene = DONE
    just need to change the next scene from 4 to whatever it ends up being

---
+++ BishopThing/Assets/Scripts/Dialogue/CutsceneOne.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Runtime.InteropServices;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class DialogueLine
+{
+    public string speaker;
+    public string text;
+}
+
+class CutsceneOne : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public void nextDialogue(string dialogue)
+    {
+        text.text = dialogue;
+    }
+
+    protected List<DialogueLine> lines = new List<DialogueLine>();
+
+    private int currentLineIndex = 0;
+
+    void Start(){
+
+        lines.Clear();
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "HEY! Stop that, shoo! Go elsewhere." });
+        lines.Add(new DialogueLine { speaker = "Teenagers", text = "Screw you old man!" });
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "Go now before the police are called!" });
+        lines.Add(new DialogueLine { speaker = "Notice", text = "*The teenagers leave the premises...*"});
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "Phew!" });
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "Hmmm, something smells off" });
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "What in the Jim Jones was that?" });
+        lines.Add(new DialogueLine { speaker = "Goblin", text = "HAHAHAHAHA, eat my curse!" });
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "What is this mysterious box that I seem to of found myself in possession of?" });
+        lines.Add(new DialogueLine { speaker = "Goblin", text = "It's Pandoras Box! You're cursed, fool!" });
+        lines.Add(new DialogueLine { speaker = "Goblin", text = "You must collect eight keys in order to open the box and be free from the curse!" });
+        lines.Add(new DialogueLine { speaker = "Goblin", text = "You can attack enemies with SPACEBAR, move with WASD, I assume you've played a game before?" });
+        lines.Add(new DialogueLine { speaker = "Goblin", text = "E to interact with things.. blah blah if you don't know this drill go start with Minecraft!" });
+        lines.Add(new DialogueLine { speaker = "Goblin", text = "Good luck, you'll need it!"});
+        lines.Add(new DialogueLine { speaker = "Bishop", text = "EURGH! My dinner will get cold!"});
+
+        currentLineIndex = 0;
+    }
+
+    public void showText()
+    {
+        if (currentLineIndex < lines.Count)
+        {
+            nextDialogue(lines[currentLineIndex].text);
+            currentLineIndex++;
+        }
+        else
+        {
+            text.text = "";
+            GoToLevelOne();
+            // if it breaks here then. Oops lol there's a glitch in the matrix
+        }
+    }
+
+    public void GoToLevelOne()
+    {
+        // TODO: CHANGE THIS SHIZ! 4 = Playground
+        SceneManager.LoadScene(4);
+    }
+}
