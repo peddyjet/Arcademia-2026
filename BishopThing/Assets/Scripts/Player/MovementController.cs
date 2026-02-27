3ec9596a529ca88594c0a12742284d9d32e814da
peddyjet <KatiePage_11@proton.me>
2026-02-27 19:06:39 +0000

    Add player movement

---
+++ BishopThing/Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _accelerationSpeed = 5f;
+    [SerializeField] private Rigidbody2D _rb;
+
+    private Vector2 _directionOfTravel;
+
+    public void OnPlayerMove(InputAction.CallbackContext input)
+    {
+        _directionOfTravel = input.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        _rb.linearVelocity = Vector2.Lerp(_rb.linearVelocity, _directionOfTravel * _speed, _accelerationSpeed * Time.deltaTime);
+    }
+}
